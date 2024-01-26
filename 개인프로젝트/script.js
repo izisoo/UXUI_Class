2919acb4a0401fe3b0b45ce2bb089889e0e81406
izisoo <jisulee74@gmail.com>
2024-01-26 18:26:21 +0900

    240126

--- 개인프로젝트/script.js
+++ 개인프로젝트/script.js
@@ -1,6 +1,7 @@
 const header = document.querySelector(".header")
 const previw_btn = document.querySelector("#preview_btn")
 const preview = document.querySelector(".preview")
+const work = document.querySelector(".work")
 
 window.addEventListener("scroll", () => {
   if(window.scrollY > 200) {
@@ -12,10 +13,13 @@ window.addEventListener("scroll", () => {
 })
 
 previw_btn.addEventListener('click', function() {
-  const isPreviewVisible = getComputedStyle(preview).display !== 'none'
-  if (getComputedStyle(preview).display !== 'none') {
-    preview.style.display = 'none';
-  } else {
-    preview.style.display = 'block';
-  }
+
+  const isPreviewVisible = (preview.style.display === 'block');
+  preview.style.display = 'block';
+  const toggleHeight = preview.offsetHeight;
+  preview.style.display = 'none';
+
+  preview.style.display = isPreviewVisible ? 'none' : 'block';
+
+  work.style.marginTop = isPreviewVisible ? '0' : (toggleHeight+100) + 'px';
 });
