9c9b840612c71a1e222c43d47e212c2efaf8138b
izisoo <jisulee74@gmail.com>
2024-01-29 18:30:21 +0900

    240129

--- 개인프로젝트/script.js
+++ 개인프로젝트/script.js
@@ -8,6 +8,8 @@ const preview_02 = document.querySelector(".preview_02")
 const previw_btn_03 = document.querySelector("#preview_btn_03")
 const preview_03 = document.querySelector(".preview_03")
 const footer = document.getElementById("#footer")
+const header_gnb_mo = document.querySelector(".header_gnb_mo")
+const header_toggle = document.querySelector(".header_toggle")
 
 window.addEventListener("scroll", () => {
   if(window.scrollY > 200) {
@@ -34,22 +36,30 @@ previw_btn_02.addEventListener('click', function() {
 
   const isPreviewVisible = (preview_02.style.display === 'block');
   preview_02.style.display = 'block';
-  const toggleHeight = preview_01.offsetHeight;
+  const toggleHeight = preview_02.offsetHeight;
   preview_02.style.display = 'none';
 
   preview_02.style.display = isPreviewVisible ? 'none' : 'block';
 
-  contact.style.marginTop = isPreviewVisible ? '0' : (toggleHeight+500) + 'px';
+  contact.style.marginTop = isPreviewVisible ? '0' : (toggleHeight+10) + 'px';
 });
 
 previw_btn_03.addEventListener('click', function() {
 
   const isPreviewVisible = (preview_03.style.display === 'block');
   preview_03.style.display = 'block';
-  const toggleHeight = preview_01.offsetHeight;
   preview_03.style.display = 'none';
 
   preview_03.style.display = isPreviewVisible ? 'none' : 'block';
 
-  footer.style.marginTop = isPreviewVisible ? '0' : (toggleHeight+200) + 'px';
+});
+
+header_toggle.addEventListener('click', function() {
+
+  const isPreviewVisible = (header_gnb_mo.style.display === 'block');
+  header_gnb_mo.style.display = 'block';
+  const toggleHeight = header_gnb_mo.offsetHeight;
+  header_gnb_mo.style.display = 'none';
+
+  header_gnb_mo.style.display = isPreviewVisible ? 'none' : 'block';
 });
