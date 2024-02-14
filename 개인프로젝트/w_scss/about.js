8b5afc10fbfcb2e01eaa1fddaf5931d1653a49ff
izisoo <jisulee74@gmail.com>
2024-02-14 21:48:13 +0900

    240214

--- 개인프로젝트/w_scss/about.js
+++ 개인프로젝트/w_scss/about.js
@@ -1,39 +1,45 @@
 // carrers
 document.addEventListener("DOMContentLoaded", function () {
-  var careersContainer = document.querySelector(".careers");
   var graphicDivs = document.querySelectorAll(".graphic .ellipse");
   var careerDivs = document.querySelectorAll(".career .company");
   var graphicIndex = 0;
   var careerIndex = 0;
-  var wheelTimeout;
+  var isAtBottom = false;
 
-  careersContainer.addEventListener("wheel", function (event) {
-    clearTimeout(wheelTimeout);
-    careersContainer.style.overflowY = "hidden";
+  window.addEventListener("scroll", function () {
+    var scrolledToBottom =
+      window.scrollY + window.innerHeight >= document.body.offsetHeight;
+    if (scrolledToBottom) {
+      isAtBottom = true;
+      document.querySelector(".graphic").addEventListener("wheel", handleWheel);
+      document.querySelector(".career").addEventListener("wheel", handleWheel);
+    } else {
+      isAtBottom = false;
+      document
+        .querySelector(".graphic")
+        .removeEventListener("wheel", handleWheel);
+      document
+        .querySelector(".career")
+        .removeEventListener("wheel", handleWheel);
+    }
+  });
 
-    wheelTimeout = setTimeout(function () {
-      careersContainer.style.overflowY = "scroll";
-    }, 100);
-
-    var deltaY = event.deltaY;
-
-    if (deltaY < 0) {
+  function handleWheel(event) {
+    if (event.deltaY < 0) {
       graphicIndex = Math.max(graphicIndex - 1, 0);
       careerIndex = Math.max(careerIndex - 1, 0);
     } else {
       graphicIndex = Math.min(graphicIndex + 1, graphicDivs.length - 1);
       careerIndex = Math.min(careerIndex + 1, careerDivs.length - 1);
     }
+    showContent(graphicDivs, graphicIndex);
+    showContent(careerDivs, careerIndex);
+    event.preventDefault();
+  }
 
-    showContent();
-  });
-
-  function showContent() {
-    graphicDivs.forEach(function (element, i) {
-      element.classList.toggle("show", i === graphicIndex);
-    });
-    careerDivs.forEach(function (element, i) {
-      element.classList.toggle("show", i === careerIndex);
+  function showContent(elements, index) {
+    elements.forEach(function (element, i) {
+      element.classList.toggle("show", i === index);
     });
   }
 });
