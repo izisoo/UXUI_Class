4485ae265865b2e60e8443f1de2418e4f66cfb99
izisoo <jisulee74@gmail.com>
2024-02-15 18:31:01 +0900

    240215

--- 개인프로젝트/w_scss/script.js
+++ 개인프로젝트/w_scss/script.js
@@ -16,11 +16,44 @@ section.forEach((section) => {
   });
 });
 
+//cursor
+// document.addEventListener("mousemove", function (e) {
+//   let cursor = document.getElementById("custom-cursor");
+//   cursor.style.display = "block";
 
+//   cursor.style.left = e.pageX + "px";
+//   cursor.style.top = e.pageY + "px";
+
+//   let element = document.elementFromPoint(e.clientX, e.clientY);
+//   let id = element.id;
+
+//   switch (id) {
+//     case "w_Graphic":
+//       cursor.querySelector("img").src = "./img/graphic.png";
+//       break;
+//     case "w_Branding":
+//       cursor.querySelector("img").src = "./img/brand.png";
+//       break;
+//     case "w_Edit":
+//       cursor.querySelector("img").src = "./img/edit.png";
+//       break;
+//     case "w_Media":
+//       cursor.querySelector("img").src = "./img/media.png";
+//       break;
+//     case "w_Web":
+//       cursor.querySelector("img").src = "./img/uxui.png";
+//       break;
+//     default:
+//       cursor.style.display = "none";
+//   }
+// });
 
 // footer
-const text = document.querySelector('.text p');
-text.innerHTML = text.innerText.split('').map(
-  (char, i) =>
-  `<span style= "transform:rotate(${i * 14.5}deg)">${char}</span>`
-).join('');
+const text = document.querySelector(".logo p");
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) =>
+      `<text style= "transform:rotate(${i * 14.5}deg)">${char}</text>`
+  )
+  .join("");
