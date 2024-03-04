1d17bc4403636f55c3cc8d864f03f5b2a2ac2841
izisoo <jisulee74@gmail.com>
2024-03-04 16:44:02 +0900

    240304

---
+++ 240304/products.js
@@ -0,0 +1,41 @@
+const products = {
+  data: [
+    {
+      id: 1,
+      name: "Cup",
+      price: 9900,
+      category: "life",
+      img: "./cup.jpg",
+    },
+    {
+      id: 2,
+      name: "mouse",
+      price: 15000,
+      category: "it",
+      img: "./mouse.jpg",
+    },
+    {
+      id: 3,
+      name: "Keyboard",
+      price: 21000,
+      category: "it",
+      img: "./keyboard.jpg",
+    },
+    {
+      id: 4,
+      name: "book",
+      price: 19000,
+      category: "book",
+      img: "./book.jpg",
+    },
+    {
+      id: 5,
+      name: "pen",
+      price: 5000,
+      category: "book",
+      img: "./pen.png",
+    },
+  ],
+};
+
+export default products;
